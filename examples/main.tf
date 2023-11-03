62e0d12610a5781ee9d56174827f3bd4b3cbcd7e
Leon Seng <w.leon.seng@gmail.com>
2023-11-03 17:07:27 +1100

    Added example

---
+++ examples/main.tf
@@ -0,0 +1,22 @@
+module "idp" {
+  source = "github.com/leonseng/terraform-azuread-saml-idp.git"
+
+  app_base_url                   = "https://azuread-saml-idp.example.com"
+  create_token_signing_cert_file = true
+}
+
+output "app_name" {
+  value = module.idp.app_name
+}
+
+output "enterprise_app_oid" {
+  value = module.idp.enterprise_app_oid
+}
+
+output "saml_idp_login_url" {
+  value = module.idp.saml_idp_login_url
+}
+
+output "saml_idp_logout_url" {
+  value = module.idp.saml_idp_logout_url
+}
