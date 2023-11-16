9f7e5d58f0a2e0d0a542137f6e3cdc10c4be8a31
Leon Seng <w.leon.seng@gmail.com>
2023-11-16 11:23:16 +1100

    Moved outputs into separate tf file

---
+++ outputs.tf
@@ -0,0 +1,24 @@
+output "app_name" {
+  value = azuread_application.this.display_name
+}
+
+output "enterprise_app_oid" {
+  value = azuread_service_principal.this.id
+}
+
+output "saml_idp_login_url" {
+  value = "https://login.microsoftonline.com/${azuread_service_principal.this.application_tenant_id}/saml2"
+}
+
+output "saml_idp_logout_url" {
+  value = "https://login.microsoftonline.com/${azuread_service_principal.this.application_tenant_id}/saml2"
+}
+
+output "saml_idp_token_signing_cert_b64" {
+  sensitive = true
+  value     = azuread_service_principal_token_signing_certificate.this.value
+}
+
+output "saml_sp_entity_id" {
+  value = local.app_entity_id
+}
